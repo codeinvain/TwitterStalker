4ef990c588b2a2d4a0ea70d3130a88e38556aee7
Daniel Cohen <daniel@codeinvain.com>
2011-10-06 19:55:08 +0200

    fixed empty users search

--- app/controllers/stalk_controller.rb
+++ app/controllers/stalk_controller.rb
@@ -15,7 +15,8 @@ class StalkController < ApplicationController
 
   def index
     if params[:q]
-      stalkees = params[:q].gsub(/\s/,',').split(',').collect{|item| item.strip.gsub(/^\@/,'')}.compact.sort
+      stalkees = params[:q].gsub(/\s/,',').split(',').collect{|item| item.strip.gsub(/^\@/,'')}.drop_while{|s| s.empty?}.sort
+      binding.pry
       @subject_count = stalkees.length
       @results = do_search(stalkees)
     end
